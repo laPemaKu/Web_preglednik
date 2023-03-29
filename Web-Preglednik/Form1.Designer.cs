40960a55d273e4dd8fc656f5ddd34feca9905995
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-03-29 09:08:16 +0200

    Add files via upload

---
+++ Web-Preglednik/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Web_Preglednik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearchBar = new System.Windows.Forms.TextBox();
+            this.webPreglednik = new System.Windows.Forms.WebBrowser();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBookmark = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(30, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(36, 22);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Go";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearchBar
+            // 
+            this.txtSearchBar.Location = new System.Drawing.Point(89, 12);
+            this.txtSearchBar.Name = "txtSearchBar";
+            this.txtSearchBar.Size = new System.Drawing.Size(1136, 20);
+            this.txtSearchBar.TabIndex = 1;
+            // 
+            // webPreglednik
+            // 
+            this.webPreglednik.Location = new System.Drawing.Point(0, 64);
+            this.webPreglednik.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webPreglednik.Name = "webPreglednik";
+            this.webPreglednik.Size = new System.Drawing.Size(1162, 617);
+            this.webPreglednik.TabIndex = 2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(89, 35);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(26, 23);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(121, 35);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(26, 23);
+            this.btnForward.TabIndex = 4;
+            this.btnForward.Text = ">";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(30, 35);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(43, 23);
+            this.btnHome.TabIndex = 5;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1222, 35);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(40, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(1171, 35);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(45, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listBookmark
+            // 
+            this.listBookmark.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.listBookmark.FormattingEnabled = true;
+            this.listBookmark.Location = new System.Drawing.Point(1168, 64);
+            this.listBookmark.Name = "listBookmark";
+            this.listBookmark.Size = new System.Drawing.Size(94, 368);
+            this.listBookmark.TabIndex = 9;
+            this.listBookmark.SelectedIndexChanged += new System.EventHandler(this.listBookmark_CLick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.listBookmark);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.webPreglednik);
+            this.Controls.Add(this.txtSearchBar);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearchBar;
+        private System.Windows.Forms.WebBrowser webPreglednik;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBookmark;
+    }
+}
+
---
+++ Web-Preglednik/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_Preglednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            webPreglednik.Navigate(txtSearchBar.Text);
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            webPreglednik.GoForward();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            webPreglednik.GoBack();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webPreglednik.Navigate("Google.com");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listBookmark.Items.Add(webPreglednik.Url.ToString());
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBookmark.Items.Clear();
+        }
+
+        private void listBookmark_CLick(object sender, EventArgs e)
+        {
+            webPreglednik.Navigate(listBookmark.SelectedItem.ToString());
+        }
+    }
+}
